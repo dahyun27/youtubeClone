de9ff3d5c91db298f26d44317e5c7c14a5f7334a
Ha Da Hyun <https://github.com/dahyun27>
2024-03-26 00:44:38 +0900

    add mixins and edit pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,37 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+const fakeUser = {
+  username: "Dahyun",
+  loggedIn: true,
+};
+
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "first video",
+      rating: 3,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "second video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "third video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+  ];
+  res.render("home", { pageTitle: "Home", fakeUser, videos });
+};
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => res.render("edit");
 export const remove = (req, res) => res.send("Remove video");
