85a0bf58560e76d150b006c594660ba22aa05fae
Ha Da Hyun <https://github.com/dahyun27>
2024-03-26 19:05:17 +0900

    edit video parts

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,37 +3,53 @@ const fakeUser = {
   loggedIn: true,
 };
 
+let videos = [
+  {
+    title: "first video",
+    rating: 3,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 1,
+    id: 1,
+  },
+  {
+    title: "second video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "third video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 67,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "first video",
-      rating: 3,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "second video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "third video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-  ];
   res.render("home", { pageTitle: "Home", fakeUser, videos });
 };
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
-export const edit = (req, res) => res.render("edit");
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("watch", { pageTitle: `Watching:  ${video.title}`, video });
+};
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+};
+
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
 export const remove = (req, res) => res.send("Remove video");
 export const upload = (req, res) => res.send("Upload Video");
 export const search = (req, res) => res.send("Search");
